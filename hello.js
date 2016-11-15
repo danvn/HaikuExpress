76468789ff867c6eafb3f36fb98d8e79e8aaf5f2
danvn <dan.nguyen7@yahoo.com>
2016-11-15 16:41:16 -0700

    POST from index.html initiates a text sent

--- hello.js
+++ hello.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser')
+var accountSid = 'ACe03012c9d0150427a0ae124bd51d96a3';
+var authToken = 'f74040cd8dde1e9e861d2022a2382cd4';
+
+
+//require the Twilio module and create a REST client
+var client = require('twilio')(accountSid, authToken);
 
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -13,16 +19,6 @@ app.get('/index.html', function(req, res){
 	res.sendFile( __dirname + "/" + "index.htm");
 })
 
-app.get('/process_get', function(req, res){
-	// Prepare output in JSON format
-	response = {
-		first_name: req.query.first_name,
-		last_name: req.query.last_name,
-		phone_number: req.query.usrtel
-	};
-	console.log(response);
-	res.end(JSON.stringify(response));
-})
 
 app.post('/process_post', urlencodedParser, function (req, res) {
    // Prepare output in JSON format
@@ -34,6 +30,15 @@ app.post('/process_post', urlencodedParser, function (req, res) {
       password:req.body.usrpwd
    };
    console.log(response);
+
+   client.messages.create({
+      to: "+17192721827",
+      from: "+13039930055",
+      body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+      }, function(err, message) {
+      console.log(message.sid);
+   });
+
    res.end(JSON.stringify(response));
 })
 
